65840ca0827864280cf986c96841bd0158e8ba67
Mohammed Shadirvan <97832799+shadirvan@users.noreply.github.com>
2026-02-20 21:07:29 +0530

    Add MathService For Performing Calculations

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,27 +1,30 @@
 import { Controller, Get, Query } from '@nestjs/common';
+import { MathService } from './math.service';
 
 @Controller()
 export class MathController {
+    constructor(private mathService: MathService) { }
+
     @Get('add')
     add(@Query('a') a: string, @Query('b') b: string) {
-        return `a is ${a} and b is ${b}`
+        return { result: this.mathService.add(+a, +b) }
     }
 
     @Get('substract')
     substract(@Query('a') a: string, @Query('b') b: string) {
-        return `a is ${a} and b is ${b}`
+        return { result: this.mathService.substract(+a, +b) }
 
     }
 
     @Get('multiply')
     multiply(@Query('a') a: string, @Query('b') b: string) {
-        return `a is ${a} and b is ${b}`
+        return { result: this.mathService.multiply(+a, +b) }
 
     }
 
     @Get('divide')
     divide(@Query('a') a: string, @Query('b') b: string) {
-        return `a is ${a} and b is ${b}`
+        return { result: this.mathService.divide(+a, +b) }
 
     }
 
