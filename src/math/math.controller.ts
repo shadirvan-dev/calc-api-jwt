b3187bb17e47417fc59316549f200b875d5fa0ce
Mohammed Shadirvan <97832799+shadirvan@users.noreply.github.com>
2026-02-20 20:52:49 +0530

    Add Endpoints in MathController for Calculation

--- src/math/math.controller.ts
+++ src/math/math.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 
-@Controller('math')
-export class MathController {}
+@Controller()
+export class MathController {
+    @Get('add')
+    add(@Query('a') a: string, @Query('b') b: string) {
+        return `a is ${a} and b is ${b}`
+    }
+
+    @Get('substract')
+    substract(@Query('a') a: string, @Query('b') b: string) {
+        return `a is ${a} and b is ${b}`
+
+    }
+
+    @Get('multiply')
+    multiply(@Query('a') a: string, @Query('b') b: string) {
+        return `a is ${a} and b is ${b}`
+
+    }
+
+    @Get('divide')
+    divide(@Query('a') a: string, @Query('b') b: string) {
+        return `a is ${a} and b is ${b}`
+
+    }
+
+}
