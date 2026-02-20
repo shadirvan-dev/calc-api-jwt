4940ba36991507e1176fae3f6061b305d843b6a5
Mohammed Shadirvan <97832799+shadirvan@users.noreply.github.com>
2026-02-20 22:13:33 +0530

    Add Boiler Plate Code For Auth Feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MathModule } from './math/math.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [MathModule],
+  imports: [MathModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
